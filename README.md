b41c59439aed3f9b8508eadd0c5e2624c6ed79e4
Harry Wolff <hswolff@gmail.com>
2015-04-10 17:18:25 -0400

    init commit

---
+++ README.md
@@ -0,0 +1,98 @@
+# gulp-slurpee [![NPM version][npm-badge-img]][npm-url] [![Dependency Status](https://david-dm.org/chartbeat-labs/gulp-slurpee.png)](https://david-dm.org/chartbeat-labs/gulp-slurpee)
+
+> Run the same [gulp][gulp-url] task from multiple gulpfile's concurrently.
+
+gulp-slurpee is useful if you have a large project with multiple applications that all have their own gulpfile, and you want to run the same task for all of them at the same time.  For instance if you wanted to lint every project concurrently without creating a meta gulp-task you would use gulp-slurpee.
+
+
+## Install
+
+Install with [npm](https://npmjs.org/package/gulp-slurpee):
+
+```sh
+npm install --global gulp-slurpee
+```
+
+
+## Usage
+
+```javascript
+gulp-slurpee <directory[/subdirectory]> [<task>]
+```
+
+If no task is supplied it uses the `default` task.
+
+
+Proxy a task to a directory:
+
+```javascript
+gulp-slurpee app
+```
+
+Proxy a task to a sub-directory:
+
+```javascript
+gulp-slurpee app/generator watch
+```
+
+Proxy a task to multiple directories:
+```javascript
+gulp-slurpee app/sites app/home build
+```
+
+If you give gulp-slurpee a directory that doesn't have a `gulpfile.js` it'll recursively look inside every child directory for a gulpfile.js and run the task for every `gulpfile.js` it finds.
+
+With a directory structure like:
+
+```sh
+- app/
+-   home/
+-     gulpfile.js
+-   about/
+-     gulpfile.js
+-   components/
+-     components.js
+```
+
+And running:
+
+```javascript
+gulp-slurpee app build
+```
+
+`gulp-slurpee` will run the `build` task in the `app/home` and `app/about` directories. It won't run the task in `app/components` as it does not have a `gulpfile.js`.
+
+
+## Changelog
+
+### 1.0
+
+- Initial release
+
+## License
+
+The MIT License (MIT)
+
+Copyright (c) 2015 Chartbeat
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+[npm-badge-img]: https://badge.fury.io/js/gulp-slurpee.png
+[npm-url]: https://npmjs.org/package/gulp-slurpee
+[gulp-url]: https://github.com/wearefractal/gulp
---
+++ index.js
@@ -0,0 +1,187 @@
+'use strict';
+
+var spawn = require('child_process').spawn;
+var path = require('path');
+var fs = require('fs');
+var util = require('util');
+var argv = require('yargs').argv;
+
+/**
+ * Looks up what gulp bin should be used.
+ * In order of priority:
+ * 	1. Global gulp
+ * 	2. Gulp local to where script is ran.
+ * 	3. Our own gulp bin.
+ */
+function getGulpBin() {
+  var globalGulp = '/usr/local/bin/gulp';
+
+  if (fs.existsSync(globalGulp)) {
+    return globalGulp;
+  }
+
+  var localGulpPath = 'node_modules/gulp/bin/gulp.js';
+
+  var localGulp = path.join(process.cwd(), localGulpPath);
+
+  if (fs.existsSync(localGulp)) {
+    return localGulp;
+  }
+
+  var selfGulp = path.join(__dirname, localGulpPath);
+
+  if (fs.existsSync(selfGulp)) {
+    return selfGulp;
+  }
+}
+
+var TASK = {
+  COMMAND: getGulpBin(),
+  FILE: 'gulpfile.js'
+};
+
+/**
+ * Does this directory have a task file?
+ * @param  {string}  directory
+ * @return {boolean}
+ */
+function taskFileExists(directory) {
+  return fs.existsSync(path.join(directory, TASK.FILE));
+}
+
+/**
+ * For a given base path this will find and return an array of all paths that
+ * contain a task file.
+ * @param  {Array.<string>} dirs Array of directory paths.
+ * @return {Array.<string>}
+ */
+function getTaskFilePaths(dirs) {
+  var directories = [];
+
+  // Loop through every directory path
+  dirs.forEach(function(dir) {
+
+    // If a task file exists in this directory and we have not
+    // yet added it to our array of directories then add it.
+    if (taskFileExists(dir) && directories.indexOf(dir) === -1) {
+      directories.push(dir);
+
+    // Ensure the directory we're accessing is a directory
+    } else if (fs.statSync(dir).isDirectory()) {
+
+      // Read all contents of the file directory and map them to have
+      // the full path to their location.
+      var filesInDir = fs.readdirSync(dir).map(function(d) {
+        return path.join(dir, d);
+      });
+
+      // Recursively call this function to gather all children directories.
+      directories = directories.concat(getTaskFilePaths(filesInDir));
+    }
+  });
+
+  return directories;
+}
+
+var childProcs = [];
+
+/**
+ * Kills all child processes.
+ * @param  {string} dirWithErr The directory where the error ocured.
+ */
+var killAllTasks = (function() {
+  var triggered = false;
+
+  return function(dirWithErr) {
+    // Only run when first invoked.
+    if (triggered) {
+      return;
+    }
+    triggered = true;
+
+    childProcs.forEach(function(child) {
+      child.kill();
+    });
+
+    console.log('\n');
+    console.log('Encountered an error in: ' + dirWithErr);
+    console.log('Please resolve errors and try again.\n');
+  };
+})();
+
+/**
+ * Spawn a task in a directory
+ * @param  {string} directory Directory to spawn task in
+ * @param  {string} task      Task we want to run
+ * @return {ChildProcess}
+ */
+function spawnTask(directory, task) {
+  var child = spawn(TASK.COMMAND, [task], {
+    cwd: directory
+  });
+
+  // Debug string used when outputting in terminal
+  // If the directory path is long then only add one tab so that
+  // all output aligns in the console.
+  var tabLength = directory.length > 22 ? '\t' : '\t\t';
+  var debugStr = util.format('(%s)%s-> ', directory, tabLength);
+
+  // Handles std{out,err} from child process.
+  function dataMsg(data) {
+    process.stdout.write(debugStr + data);
+  }
+
+  child.stdout.on('data', dataMsg);
+  child.stderr.on('data', dataMsg);
+
+  child.on('exit', function(code) {
+    // If the child process exits abnormally then the code value will
+    // not be 0.
+    if (code !== 0) {
+      killAllTasks(directory);
+      process.exit(1);
+    }
+  });
+
+  return child;
+}
+
+/**
+ * Exported function that begins the process of spawning individual gulp tasks.
+ */
+exports.run = function() {
+  var task = '';
+  var argDirectories;
+  var lastArgv = argv._.pop();
+
+  /**
+   * Guess what our task is.
+   * We check the last process argument value to see if
+   * it exists on the file system, and if it does
+   * then the last argument is a directory and hence
+   * we should use the `default` task.
+   */
+  if (fs.existsSync(lastArgv)) {
+    task = 'default';
+    // Re-join full directory list
+    argDirectories = argv._.concat(lastArgv);
+  } else {
+    task = lastArgv;
+    // Ensure always array
+    argDirectories = [].concat(argv._);
+  }
+
+  // Array of directories we will run our task in.
+  var taskDirectories = getTaskFilePaths(argDirectories);
+
+  console.log('Running cmd:   ' + TASK.COMMAND + ' ' + task);
+  console.log('In directories: ');
+  taskDirectories.forEach(function(dir) {
+    console.log('           ' + dir);
+  });
+  console.log('\n----------\n');
+
+  taskDirectories.forEach(function(dir) {
+    childProcs.push(spawnTask(dir, task));
+  });
+};
